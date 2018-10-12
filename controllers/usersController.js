858612a24f7edccb9d9e7412db85d82ab669475c
Natalia C <natalia.calt@gmail.com>
2018-10-11 21:51:33 -0600

    token to verify

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -39,26 +39,6 @@ router.post('/register', function(req, res) {
   });
 });
 
-//Check User Access Token //NEEDS CLARIFICATION!!!
-// const authenticationMiddleware = (req, res, next) {
-//   // console.log(req.body, 'REQ');
-//   // console.log(res.body, "RES");
-//   var token = req.headers['x-access-token'];
-//   if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
-//
-//   jwt.verify(token, authSecret.secret, function(err, decoded) {
-//     if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-//
-//     User.findById(decoded.id,
-//       { password: 0 }, // projection
-//       function (err, user) {
-//         if (err) return res.status(500).send("There was a problem finding the user.");
-//         if (!user) return res.status(404).send("No user found.");
-//
-//         next();
-//     });
-//   });
-// }
 
 //LOGIN
 //req.body = { email: 'natalia_calt@yahoo.com', password: 'password' }
