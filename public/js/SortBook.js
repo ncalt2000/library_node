e6dec28f6808c07db243aa73c18f8352498a07ae
Natalia C <natalia.calt@gmail.com>
2018-10-20 18:17:24 -0600

    code cleaned

--- public/js/SortBook.js
+++ public/js/SortBook.js
@@ -24,30 +24,6 @@ class SortBook {
     window.gDataTable._bindEvents();
   }
 
-  // _bindEvents() {
-  //   console.log("Bind events from SortBook!!!");
-  //   $('#sort-title').on('click', this._sortBy.bind(this));
-  //   $('#sort-author').on('click', this._sortBy.bind(this));
-  //   $('#sort-genre').on('click', this._sortBy.bind(this));
-  //   $('#sort-rating').on('click', this._sortBy.bind(this));
-  // }
-
-  // _sortBy(e) {
-  //   const val = $(e.target).data('sort');
-  //   // console.log(val, "VALUE");
-  //   this.allBooks.sort((a, b) => {
-  //     if (typeof a[val] === 'number') {
-  //       return b[val] - a[val];
-  //     }
-  //     const itemA = a[val].toLowerCase();
-  //     const itemB = b[val].toLowerCase();
-  //     if (itemA < itemB) // sort string ascending
-  //     { return -1; }
-  //     if (itemA > itemB) return 1;
-  //     return 0; // default return value (no sorting)
-  //   });
-  //   this._reload();
-  // }
 }
 
 $(() => {
